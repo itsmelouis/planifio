9dc0f2e8bceb46371e8a73da84c60be1b6de8947
Louis F. <72155852+itsmelouis@users.noreply.github.com>
2025-02-24 10:12:41 +0100

    feat(PWA): add PWA install

---
+++ app/pages/about.vue
@@ -0,0 +1,1 @@
+<template><div>salut</div></template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+const router = useRouter();
 </script>
 
 <template>
-  <Button>Button</Button>
+  <Button @click="router.go('about')">Button</Button>
 </template>
