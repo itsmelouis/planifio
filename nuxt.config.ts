a02aeba1334960fb22c38d132c8bab6a9853157c
Louis F. <72155852+itsmelouis@users.noreply.github.com>
2025-02-24 09:46:48 +0100

    chore: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  modules: ['@vite-pwa/nuxt', 'shadcn-nuxt', '@nuxtjs/tailwindcss'],
+
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
